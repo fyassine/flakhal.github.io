92bbaddb5137f85e11d6565e9a2dc0b735f87af2
Fraj Yassine Lakhal <flakhal@Yassines-MacBook-Pro.local>
2025-03-27 18:28:50 +0100

    [feat] Write content

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <section className="about">
-    <p>
-      Hello! This is my website!
-    </p>
-  </section>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
